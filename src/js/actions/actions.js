6f05a0ac73008300b7f47178b83af1abea4d875d
maxadamsdes <62082220+maxadamsdes@users.noreply.github.com>
2020-09-10 14:54:37 +1200

    Finished tutorial

---
+++ src/js/actions/actions.js
@@ -0,0 +1,23 @@
+import Flux from "@4geeksacademy/react-flux-dash";
+import store from '../stores/store';
+export const addContact = (contact) => {
+    let contacts = store.getState('contacts');
+    if(!contacts) contacts = [contact];
+    else contacts.push(contact);
+    Flux.dispatchEvent("contacts", contacts);
+};
+export const deleteContact = (contact) => {
+    let contacts = store.getState('contacts');
+    const newListOfContacts = contacts.filter((c) => (c.id != contact.id));
+    Flux.dispatchEvent("contacts", newListOfContacts);
+};
+export const editContact = (contact) => {
+    let contacts = store.getState('contacts');
+    const newListOfContacts = contacts.map((c) => {
+        if(c.id == contact.id) return contact;
+        return c;
+    });
+    Flux.dispatchEvent("contacts", newListOfContacts);
+};
+window.addContact = addContact;
+window.deleteContact = deleteContact;
--- src/js/views/AddContact.jsx
+++ src/js/views/AddContact.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import Flux from "@4geeksacademy/react-flux-dash";
 import { Link } from "react-router-dom";
+import * as actions from '../actions/actions';
+import store from '../stores/store';
 
-export default class Contacts extends Flux.View {
+export default class Contacts extends Flux.DashView {
+    constructor(){
+        super();
+        this.state = {
+            full_name: '',
+            mode: 'add'
+        };
+    }
+    componentDidMount(){
+        if(typeof this.props.match.params.id !== 'undefined'){
+            const contacts = store.getState('contacts'); 
+            const contact = contacts.find((c) => c.id == this.props.match.params.id);
+            this.setState({mode: 'edit', full_name: contact.full_name, id: contact.id });
+        }
+
+        this.subscribe(store, "contacts", (contacts) => {
+            this.props.history.push('/');
+        });
+    }
     render() {
         return (
             <div className="container">
@@ -11,9 +31,25 @@ export default class Contacts extends Flux.View {
                     <form>
                         <div className="form-group">
                             <label>Full Name</label>
-                            <input type="text" className="form-control" placeholder="Full Name" />
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Full Name"
+                                onChange={(e) => this.setState({
+                                    full_name: e.target.value
+                                })}
+                                value={this.state.full_name}
+                                />
                         </div>
-                        <button type="button" className="btn btn-primary form-control">save</button>
+                        <button
+                            type="button"
+                            className="btn btn-primary form-control"
+                            onClick={() => {
+                                if(this.state.mode == 'add') actions.addContact({full_name: this.state.full_name});
+                                else if(this.state.mode == 'edit') actions.editContact({full_name: this.state.full_name, id: this.state.id});
+                            }
+                        }
+                        >save</button>
                         <Link className="mt-3 w-100 text-center" to="/">or get back to contacts</Link>
                     </form>
                 </div>
